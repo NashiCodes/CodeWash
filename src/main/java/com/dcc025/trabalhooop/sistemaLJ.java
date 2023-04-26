828e7442f074252be834955719a4f235c578ba77
Perera Dev <nashi.codes@gmail.com>
2023-04-26 02:00:20 -0300

    esqueleto do SistemaLJ implementado

--- src/main/java/com/dcc025/trabalhooop/sistemaLJ.java
+++ src/main/java/com/dcc025/trabalhooop/sistemaLJ.java
@@ -1,9 +1,44 @@
 package com.dcc025.trabalhooop;
 
+import java.util.*;
+
 public class sistemaLJ {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("//----------------------------------------------------------------//");
+			System.out.println("			Bem vindo ao SistemaLJ, o que deseja fazer?");
+			System.out.println("	(1) Cadastrar seu Lava Jato");
+			System.out.println("	(2) Cadastrar um novo serviço");
+			System.out.println("	(-1) Sair");
+			System.out.println("//----------------------------------------------------------------//");
+			int option = scan.nextInt();
+			seletor(option);
+			while (option != -1) {
+				seletor(option);
+				option = scan.nextInt();
+			}
+		}
+
 	}
 
+	public static void seletor(int option) {
+		switch (option) {
+			case 1:
+				NovoLJ();
+				break;
+			case 2:
+				NovoProduto();
+			default:
+				break;
+		}
+	}
+
+	public static void NovoLJ() {
+
+	}
+
+	public static void NovoProduto() {
+
+	}
 }
