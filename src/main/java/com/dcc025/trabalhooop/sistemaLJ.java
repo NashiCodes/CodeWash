17d0b1cc324c332a1b151697ac0a89c08e3dde6a
Perera Dev <nashi.codes@gmail.com>
2023-05-06 16:25:35 -0300

    corrigindo bugs e refatorando o codigo, função main dividida em varios componentes

--- src/main/java/com/dcc025/trabalhooop/sistemaLJ.java
+++ src/main/java/com/dcc025/trabalhooop/sistemaLJ.java
@@ -4,50 +4,68 @@ import java.util.*;
 
 public class sistemaLJ {
 
-	private static Vector<Place> places = new Vector<Place>();
+	private static List<Place> places = new ArrayList<Place>();
+	private static String option;
+	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		option = "1";
+		menu();
+	}
 
-		try (Scanner scan = new Scanner(System.in)) {
+	private static void menu() {
+		while (option != "-1") {
+			menuOptions();
+			option = leitorOptions();
+			seletor();
+		}
+	}
+
+	private static String leitorOptions() {
+		option = scan.nextLine();
+		return option;
+	}
+
+	private static void menuOptions() {
+		if (places.isEmpty()) {
 			System.out.println("//----------------------------------------------------------------//");
 			System.out.println("			Bem vindo ao SistemaLJ, o que deseja fazer?");
 			System.out.println("	(1) Cadastrar seu Lava Jato");
-			System.out.println("	(2) Cadastrar um novo serviço");
 			System.out.println("	(-1) Sair");
-			System.out.println("//----------------------------------------------------------------//");
-			int option = scan.nextInt();
-			seletor(option);
-			option = scan.nextInt();
-			while (option != -1) {
-				seletor(option);
-				option = scan.nextInt();
-			}
-		}
+		} else
+			System.out.println("	(2) Cadastrar um novo serviço");
 
+		System.out.println("//----------------------------------------------------------------//");
 	}
 
-	public static void seletor(int option) {
+	public static void seletor() {
 		switch (option) {
-			case 1:
+			case "1":
 				novoLJ();
 				break;
-			case 2:
+			case "2":
 				novoProduto();
-			case -1:
+			case "-1":
 				break;
 		}
 	}
 
 	public static void novoLJ() {
-		try (Scanner scan = new Scanner(System.in)) {
-			System.out.println("Digite o nome do seu Lava Jato: ");
-			final String nome = scan.nextLine();
-			places.addElement(new Place(nome));
+		System.out.println("Digite o nome do seu Lava Jato: ");
+		String nome = leitorString();
+		places.add(new Place(nome));
+		for (Place place : places) {
+			System.out.println(place.getName());
 		}
 	}
 
 	public static void novoProduto() {
 
+	}
+
+	private static String leitorString() {
+		String nome = scan.nextLine();
+		return nome;
 	}
 
 }
