c4bd2564d3506121918637cae2cf3c136c799f91
GabrielVieira9768 <gabrieloliveira.vieira@estudante.ufjf.br>
2023-05-08 19:24:52 -0300

    Implementa métodos dos produtos - adicionar, remover e editar

--- src/main/java/com/dcc025/trabalhooop/Administrador.java
+++ src/main/java/com/dcc025/trabalhooop/Administrador.java
@@ -3,8 +3,26 @@ package com.dcc025.trabalhooop;
 import java.util.*;
 
 public class Administrador extends Usuario{
+    private HashMap<String, Double> produtos;
 
     public Administrador(String nome, String telefone, String email) {
         super(nome, telefone, email);
+        this.produtos = new HashMap<String, Double>();
+    }
+
+    public void adicionarProduto(String nome, double preco) {
+        this.produtos.put(nome, preco);
+    }
+
+    public void removerProduto(String nome) {
+        this.produtos.remove(nome);
+    }
+
+    public void editarPrecoProduto(String nome, double novoPreco) {
+        if (this.produtos.containsKey(nome)) {
+            this.produtos.put(nome, novoPreco);
+        } else {
+            System.out.println("O produto " + nome + " não está cadastrado.");
+        }
     }
 }
