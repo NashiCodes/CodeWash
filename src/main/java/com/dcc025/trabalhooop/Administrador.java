82d2357ce95d46720e860cc304a78f71f3918989
GabrielVieira9768 <gabrieloliveira.vieira@estudante.ufjf.br>
2023-05-08 19:21:46 -0300

    Cria classe Administrador

---
+++ src/main/java/com/dcc025/trabalhooop/Administrador.java
@@ -0,0 +1,10 @@
+package com.dcc025.trabalhooop;
+
+import java.util.*;
+
+public class Administrador extends Usuario{
+
+    public Administrador(String nome, String telefone, String email) {
+        super(nome, telefone, email);
+    }
+}
