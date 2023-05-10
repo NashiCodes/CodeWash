b3eb109a397881e670a237e63c8d1b78a7a5acba
Perera Dev <nashi.codes@gmail.com>
2023-05-10 02:19:20 -0300

    limpando a Main

--- src/main/java/com/dcc025/trabalhooop/sistemaLJ.java
+++ src/main/java/com/dcc025/trabalhooop/sistemaLJ.java
@@ -4,16 +4,15 @@ import java.util.*;
 
 public class sistemaLJ {
 
-	private static final List<Place> places = new ArrayList<>();
 	private static final Scanner scan = new Scanner(System.in);
 	private static Usuario usuario;
 	private static final List<Cadastro> cadastro = new ArrayList<Cadastro>();
+	
 	private static String option;
-	private static boolean logado = false;
 
 	private static String leitor() {
-		String nome = scan.nextLine();
-		return nome;
+		String string = scan.nextLine();
+		return string;
 	}
 
 	public static void main(String[] args) {
@@ -32,31 +31,13 @@ public class sistemaLJ {
 	private static void menuOptions() {
 		System.out.println("//----------------------------------------------------------------//");
 		System.out.println("		Bem vindo ao SistemaLJ, o que deseja fazer?");
-
-		if (!logado) {
-			System.out.println("(1)Cadastre-se");
-			System.out.println("(2)login");
-		} else {
-			System.out.println("	(1) Cadastrar seu Lava Jato");
-			System.out.println("	(2) Cadastrar um novo serviço");
-		}
-
-		System.out.println("	(-1) Sair");
+		System.out.println("(1)Cadastre-se");
+		System.out.println("(2)login");
+		System.out.println("(-1) Sair");
 		System.out.println("//----------------------------------------------------------------//");
 	}
 
 	public static void seletor() {
-		if (logado) {
-			switch (option) {
-				case "1":
-					novoLJ();
-					break;
-				case "2":
-					novoProduto();
-				default:
-					break;
-			}
-		} else {
 			switch (option) {
 				case "1":
 					cadastrar();
@@ -65,40 +46,12 @@ public class sistemaLJ {
 					login();
 				default:
 					break;
-			}
-		}
-	}
-
-	public static void novoLJ() {
-		System.out.println("Digite o nome do seu Lava Jato:");
-		String nome = leitor();
-		places.add(new Place(nome));
-	}
-
-	public static void novoProduto() {
-		System.out.println("Digite o nome do seu Lava Jato:");
-		String nome = leitor();
-		for (Place place : places) {
-			if (place.getName().equals(nome)) {
-				System.out.println("Digite o nome do seu produto:");
-				nome = leitor();
-				System.out.println("Digite o preço do seu produto:");
-				String preço = leitor();
-				place.addProdutos(new Servicos(nome, Integer.parseInt(preço)));
-				break;
-			} else if (places.indexOf(place) == places.size() - 1) {
-				System.out.println("Não foi possivel adicionar o Produto");
-				break;
-			}
-
-		}
-
+			}		
 	}
 
 	public static void cadastrar() {
 		menuCadastro();
 		cadastro.add(new Cadastro(usuario.getEmail(), usuario.getSenha(cadastro), usuario.getClass().getName()));
-		logado = true;
 	}
 
 	public static void menuCadastro() {
@@ -122,9 +75,6 @@ public class sistemaLJ {
 			default:
 				break;
 		}
-	}
-
-	public static void seletorCad(String tipoUser) {
 	}
 
 	public static void login() {
