a5e9f53827b4dbf9fd14218cc0fe296721a3dae2
Perera Dev <nashi.codes@gmail.com>
2023-05-09 23:39:24 -0300

    função que retorna os horarios indisponiveis

--- src/main/java/com/dcc025/trabalhooop/Horario.java
+++ src/main/java/com/dcc025/trabalhooop/Horario.java
@@ -4,21 +4,31 @@ import java.util.*;
 
 public class Horario {
 
-   private ArrayList<HashMap<String, Integer>> horarios;// Array de hashMap para armazenar
-                                                        // os horarios referentes de cada dia da semana
+   private List<HashMap<String, Integer>> horarios;// Array de hashMap para armazenar
+                                                   // os horarios referentes de cada dia da semana
    private final int Dias_de_Funcionamento; // varivel para armazenar os dias da semana
                                             // que o lava jato funciona
 
+   private final List<Integer> horario_de_Funcionamento; // Vetor que armazena os Horarios de Funcionamento
+
    public Horario(int dias) {
       this.Dias_de_Funcionamento = dias;
+      this.horario_de_Funcionamento = new ArrayList<Integer>(2);
+      initHF();
       horarios = new ArrayList<HashMap<String, Integer>>(this.Dias_de_Funcionamento);
       for (int i = 0; i < this.Dias_de_Funcionamento; i++) {
          horarios.add(new HashMap<String, Integer>()); // inicializa todos os horários
                                                        // com um HashMap vazio
       }
+
    }
 
-   public void setHorario(int diaSemana, String horario, String nome) throws HorarioJaOcupadoException {
+   private void initHF() {
+      for (int i = 8; this.horario_de_Funcionamento.size() < 2; i += 10)
+         setHF(i);
+   }
+
+   public void setHorario(int diaSemana, String horario) throws HorarioJaOcupadoException {
       if (diaSemana >= 0 && diaSemana < this.Dias_de_Funcionamento) { // se o dia selecionado pelo usuario for valido
          HashMap<String, Integer> horariosDia = horarios.get(diaSemana);// pega todos os horarios desse dia
          if (horariosDia.containsKey(horario)) {// verifica se nesse dia outra pessoa ja seleciou tal horario
@@ -50,6 +60,23 @@ public class Horario {
       } else {
          return "Dia selecionado invalido!!";
       }
+   }
+
+   public ArrayList<String> getHhrsIndisponiveis(int diaSemana) {
+      ArrayList<String> horariosIndisponiveis = new ArrayList<String>();
+      if (diaSemana >= 0 && diaSemana < this.Dias_de_Funcionamento) { // se for um dia valido
+         HashMap<String, Integer> horariosDia = horarios.get(diaSemana);// pega todos os horarios desse dia
+         for (String horario : horariosDia.keySet()) {// percorre todos os horariosdesse dia
+            if (horariosDia.get(horario) == 2) {// verifica se esse horario ainda tem 2 pessoas marcadas
+               horariosIndisponiveis.add(horario);// adiciona esse horario na lista de horarios indisponiveis
+            }
+         }
+      }
+      return horariosIndisponiveis;
+   }
+
+   public void setHF(int hr) {
+      this.horario_de_Funcionamento.add(hr);
    }
 
    public static class HorarioJaOcupadoException extends Exception { // classe estatica que retorna a mensagem de erro
